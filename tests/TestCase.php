62d725db148788013c15781f39ed131278b4d052
Paulo Real <contact@pauloreal.com>
2020-11-13 08:35:42 -0300

    feat(tests/testCase): add create method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,21 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
     /**
+     * Call factory directly
+     *
+     * @param  mixed $model
+     * @param  mixed $data
+     * @return Model
+     */
+    protected function create(string $model, $data = null): Model
+    {
+        $model = new $model();
+        $data = (!empty($data) && count($data)) ? $data : null;
+
+        return $model->factory()->create($data);
+    }
+
+    /**
      * Authenticate user to run an test
      *
      * @param  mixed $user
