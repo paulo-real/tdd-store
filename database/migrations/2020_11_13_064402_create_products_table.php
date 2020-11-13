ffc1556e93a3418996a613adc9ad8009eaf38224
Paulo Real <contact@pauloreal.com>
2020-11-13 10:43:32 -0300

    style(migrations/product): remove blank space

--- database/migrations/2020_11_13_064402_create_products_table.php
+++ database/migrations/2020_11_13_064402_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
-            $table->double('price'); 
+            $table->double('price');
             $table->timestamps();
         });
     }
