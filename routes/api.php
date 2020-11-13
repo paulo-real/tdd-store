2e867437b342a7ee6edd6291ade753eeaee03aa2
Paulo Real <contact@pauloreal.com>
2020-11-13 11:47:59 -0300

    feat(routes/api): add customer resource

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('product', 'App\Http\Controllers\ProductController');
+Route::resource('customer', 'App\Http\Controllers\CustomerController');
