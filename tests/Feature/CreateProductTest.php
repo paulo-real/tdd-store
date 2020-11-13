7a474f098c945316479aa7831ddb719e21a5a95a
Paulo Real <contact@pauloreal.com>
2020-11-13 09:42:08 -0300

    tests(feature/createProduct): add new

---
+++ tests/Feature/CreateProductTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateProductTest extends TestCase
+{
+    protected $model = Product::class;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testAuthenticatedUserCanCreateProducts()
+    {
+        $this->auth();
+
+        $product = Product::factory()->make();
+
+        $response = $this->post('/api/product', $product->toArray());
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas($this->table, [
+            'name' => $product->name,
+            'description' => $product->description,
+        ]);
+    }
+}
