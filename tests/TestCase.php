aa6f575bf11199bacab147eacf37c88259e9d063
Paulo Real <contact@pauloreal.com>
2020-11-13 08:06:39 -0300

    feat(tests/testCase): add authenticate method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,20 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Authenticate user to run an test
+     *
+     * @param  mixed $user
+     * @return void
+     */
+    protected function auth($user = null)
+    {
+        $user = $user ?: $this->create('App\Models\User');
+
+        return auth()->attempt([
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+    }
 }
