f91e97c1d3527692dd1345dcf2ef76e9a7358f83
Paulo Real <contact@pauloreal.com>
2020-11-13 09:42:49 -0300

    feat(requests/product): add new

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest
+class ProductRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
